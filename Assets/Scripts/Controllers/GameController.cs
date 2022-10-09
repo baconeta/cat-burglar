8d92250b9a65e3e80f1b6fa92d5b3336b6cf0e7b
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2022-10-10 00:10:04 +0300

    refactor some scripts in prep for audio controller persistence

---
+++ Assets/Scripts/Controllers/GameController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace Controllers
+{
+    public class GameController : MonoBehaviour
+    {
+        private void Start()
+        {
+            GameWindowManagement();
+        }
+
+        private static void GameWindowManagement()
+        {
+            Cursor.lockState = CursorLockMode.Locked; // has an issue with mouse delta spikes in frame 1
+            Cursor.visible = false;
+        }
+    }
+}
